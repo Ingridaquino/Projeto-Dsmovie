a2eed06d562b41c41d56ecb93eb6d068ccf43cad
Ingrid Aquino <ingridaquinosoares@live.com>
2022-03-31 15:16:17 -0300

    :beers: Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,26 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import Listing from "pages/Listing";
+import Form from "pages/Form";
+
 import Header from "./components/Header";
 
 
 function App() {
   return (
-    < Header />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/Form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
