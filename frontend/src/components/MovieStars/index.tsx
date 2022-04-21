77ef362deaed1b98ba63dc689309c06ed88a4002
Ingrid Aquino <ingridaquinosoares@live.com>
2022-04-21 14:56:39 -0300

    :construction: Show score

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -1,14 +1,64 @@
 import {BsStarFill, BsStarHalf, BsStar} from "react-icons/bs";
 import "./styles.css";
 
-function MovieStars() {
+
+type Props = {
+    score: number;
+}
+
+type StarProps = {
+    fill: number;
+}
+
+
+//Exemplo
+//getFills(3.5) => [1, 1, 1, 0.5, 0]
+//getFills(4.1) => [1, 1, 1, 1, 0.5]
+
+
+//Função que irá defini as estrela de acordo com a nota do filme
+function getFills(score: number) {
+
+    const fills = [0, 0, 0, 0, 0,];
+
+    const integerPart = Math.floor(score);
+
+    for (let i = 0; i < integerPart; i++){
+        fills[i] = 1;
+    }
+
+    const diff = score - integerPart;
+    if(diff > 0) {
+        fills[integerPart] = 0.5;
+    }
+
+    return fills;
+}
+
+function Star( {fill} : StarProps){
+    if(fill === 0) {
+        return <BsStar />
+    }
+    else if(fill === 1){
+        return <BsStarFill />
+    }
+    else {
+        return <BsStarHalf />
+    }
+}
+
+
+function MovieStars({ score } : Props) {
+
+    const fills = getFills(score);
+
     return (
         <div className="dsmovie-stars-container">
-            <BsStarFill />
-            <BsStarFill />
-            <BsStarFill />
-            <BsStarHalf />
-            <BsStar />
+            <Star fill={fills[0]} />
+            <Star fill={fills[0]} />
+            <Star fill={fills[0]} />
+            <Star fill={fills[0]} />
+            <Star fill={fills[0]} />
         </div>
     );
 }
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -44,11 +44,12 @@ function Listing() {
 
     return (
         <>
-            <Pagination page={page} onChange={handlePageChange}/>
+           
+      <Pagination page={page} onChange={handlePageChange}/>
 
             <div className="container">
                 <div className="row">
-                    {page.content.map(movie => (
+                    {page.content.map((movie) => (
                     <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard movie={movie}/>
                     </div>
